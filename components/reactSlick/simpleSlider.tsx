4a770fe6c77da96fa1dc363d6728e702253268d1
kjkim <kjkim@xsinc.co.kr>
2024-03-09 23:33:41 +0900

    refactor: simpleSlider 컴포넌트 props 최적화

--- components/reactSlick/simpleSlider.tsx
+++ components/reactSlick/simpleSlider.tsx
@@ -5,27 +5,12 @@ import simpleSliderStyled from "@styles/components/reactSlider/simpleSlider.modu
 
 export default function SimpleSlider({
   children,
-  onChangeIdx,
   ...props
 }: {
   children: React.ReactNode;
-  onChangeIdx?: (idx: number) => void;
 }) {
-  const settings = {
-    dots: true,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    afterChange: (idx: any) => {
-      if (onChangeIdx) onChangeIdx(idx);
-    },
-  };
-
   return (
-    <Slider className={simpleSliderStyled.slider} {...settings} {...props}>
+    <Slider className={simpleSliderStyled.slider} {...props}>
       {children}
     </Slider>
   );
