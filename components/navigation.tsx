520fa9fd6e338276347e3a2795c1c0a907fcd78d
kjkim <kjkim@xsinc.co.kr>
2024-03-13 18:04:58 +0900

    refactor: 내비게이션 항목 추가

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -6,7 +6,8 @@ export default function Navigation() {
   return (
     <div className={navigationStyles.navigation_container}>
       <Link href="/">홈</Link>
-      <Link href="/product">상품</Link>
+      <Link href="/allProduct">전체 상품</Link>
+      <Link href="/timeSaleProduct">타임 특가</Link>
       <Link href="/cart">장바구니</Link>
       <Link href="/payment">결제</Link>
     </div>
