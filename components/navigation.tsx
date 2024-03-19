df35f671c727c02326bca42d200d727d35b78964
kjkim <kjkim@xsinc.co.kr>
2024-03-19 09:49:16 +0900

    test: 내비게이션 임시 항목 추가

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -10,6 +10,7 @@ export default function Navigation() {
       <Link href="/timeSaleProduct">타임 특가</Link>
       <Link href="/productDetail">상품 상세</Link>
       <Link href="/cart">장바구니</Link>
+      <Link href="/order">주문하기</Link>
       <Link href="/payment">결제</Link>
     </div>
   );
