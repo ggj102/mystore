2e068aca0611f726a877ad0bb4c62cd40c978797
kjkim <kjkim@xsinc.co.kr>
2024-03-22 11:25:28 +0900

    refactor: menuToggle dom 구조 변경

--- components/header/navigation/menuToggle.tsx
+++ components/header/navigation/menuToggle.tsx
@@ -56,28 +56,30 @@ export default function MenuToggle({ isNavOpen, toggle }: any) {
   }, [isNavOpen]);
 
   return (
-    <div ref={scope} className={menuToggle.menu_toggle_container}>
-      <button onClick={toggle}>
-        <svg width="23" height="18" viewBox="0 0 23 18">
-          <Path
-            d="M 2 2.5 L 20 2.5"
-            className="top"
-            variants={{
-              closed: { d: "M 2 2.5 L 20 2.5" },
-              open: { d: "M 3 16.5 L 17 2.5" },
-            }}
-          />
-          <Path d="M 2 9.423 L 20 9.423" opacity="1" className="middle" />
-          <Path
-            d="M 2 16.346 L 20 16.346"
-            className="bottom"
-            variants={{
-              closed: { d: "M 2 16.346 L 20 16.346" },
-              open: { d: "M 3 2.5 L 17 16.346" },
-            }}
-          />
-        </svg>
-      </button>
-    </div>
+    <button
+      ref={scope}
+      className={menuToggle.menu_toggle_container}
+      onClick={toggle}
+    >
+      <svg width="23" height="18" viewBox="0 0 23 18">
+        <Path
+          d="M 2 2.5 L 20 2.5"
+          className="top"
+          variants={{
+            closed: { d: "M 2 2.5 L 20 2.5" },
+            open: { d: "M 3 16.5 L 17 2.5" },
+          }}
+        />
+        <Path d="M 2 9.423 L 20 9.423" opacity="1" className="middle" />
+        <Path
+          d="M 2 16.346 L 20 16.346"
+          className="bottom"
+          variants={{
+            closed: { d: "M 2 16.346 L 20 16.346" },
+            open: { d: "M 3 2.5 L 17 16.346" },
+          }}
+        />
+      </svg>
+    </button>
   );
 }
