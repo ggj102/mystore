bf552615352361c220d015b9ca5f9533240b2d70
kjkim <kjkim@xsinc.co.kr>
2024-05-17 17:50:05 +0900

    chore: next.config cors 관련 설정

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,19 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  async headers() {
+    return [
+      {
+        // 모든 경로에 대해 CORS 정책 설정
+        source: "/(.*)",
+        headers: [
+          {
+            key: "Access-Control-Allow-Origin",
+            value: "*", // 특정 도메인 또는 '*'로 모든 도메인 허용
+          },
+        ],
+      },
+    ];
+  },
+};
 
 export default nextConfig;
