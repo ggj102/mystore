bc7ca1ebc0909217d34c1c36e9ff2b4541abe6d0
kjkim <kjkim@xsinc.co.kr>
2024-05-23 21:19:39 +0900

    refactor: [Signin] form 추가 및 에러 메시지 추가 - yup로 유효성 검사 - react-hook-form 사용 - 아이디, 비밀번호 불일치 알림

--- app/signin/components/signin.tsx
+++ app/signin/components/signin.tsx
@@ -1,47 +1,56 @@
 "use client";
 
-import { useState } from "react";
 import { useRouter } from "next/navigation";
+import { useForm } from "react-hook-form";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+import * as yup from "yup";
 
 import api from "@/httpClient/auth";
 
-export default function Signin() {
-  const [currentId, setCurrentId] = useState<string>("");
-  const [currentPassword, setCurrentPassword] = useState<string>("");
+const schema = yup.object().shape({
+  user_id: yup.string().required("아이디를 입력해주세요."),
+  user_password: yup.string().required("비밀번호를 입력해주세요."),
+});
 
+export default function Signin() {
   const router = useRouter();
 
-  const onClickLognin = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = (data: { user_id: string; user_password: string }) => {
     api
-      .post("http://localhost:3005/signin", {
-        user_id: currentId,
-        user_password: currentPassword,
-      })
+      .post("http://localhost:3005/signin", { ...data })
       .then((res) => {
         if (res.data.accessToken) {
           localStorage.setItem("accessToken", res.data.accessToken);
           router.replace("/");
         }
+      })
+      .catch((err) => {
+        alert(err.response.data.message);
       });
   };
 
   return (
-    <div>
+    <form onSubmit={handleSubmit(onSubmit)}>
       <div>
         <span>아이디</span>
-        <input
-          value={currentId}
-          onChange={(e) => setCurrentId(e.target.value)}
-        />
+        <input {...register("user_id")} />
+        {errors.user_id && <p>{errors.user_id.message}</p>}
       </div>
       <div>
         <span>비밀번호</span>
-        <input
-          value={currentPassword}
-          onChange={(e) => setCurrentPassword(e.target.value)}
-        />
+        <input {...register("user_password")} />
+        {errors.user_password && <p>{errors.user_password.message}</p>}
       </div>
-      <button onClick={onClickLognin}>로그인</button>
-    </div>
+      <button type="submit">로그인</button>
+    </form>
   );
 }
