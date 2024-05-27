f0b9d1e2d273c3af1e5a6b97b55d064fcf9755d5
kjkim <kjkim@xsinc.co.kr>
2024-05-27 23:46:06 +0900

    refactor: mainLayout redux 로직 삭제

--- components/mainLayout.tsx
+++ components/mainLayout.tsx
@@ -1,22 +1,9 @@
 "use client";
 
-import { useAppDispatch } from "@/src/adaptter/redux/hooks";
-import { initUserAction } from "@/src/adaptter/redux/reducer/userReducer";
-import axios from "axios";
-import { useEffect } from "react";
-
 export default function MainLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  const dispatch = useAppDispatch();
-
-  useEffect(() => {
-    axios.get("http://localhost:3005/testUserInfo").then((res: any) => {
-      dispatch(initUserAction(res.data));
-    });
-  }, []);
-
   return <div>{children}</div>;
 }
