b0a8a631d50a98dcbed7727b662a3dc4d33d7e67
kjkim <kjkim@xsinc.co.kr>
2024-06-05 22:35:59 +0900

    style: typo

--- httpClient/errorMessage.ts
+++ httpClient/errorMessage.ts
@@ -3,5 +3,5 @@ export const tokenExpiredErrorMessage = (err: any) => {
     alert("로그인이 필요한 서비스 입니다.");
 
     window.location.pathname = "/signin";
-  } else alert(`Error: ${err.message}`);
+  } else alert(err.message);
 };
