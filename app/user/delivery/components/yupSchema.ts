905828479cf61a30216788110c7e1f384525f79b
kjkim <kjkim@xsinc.co.kr>
2024-06-05 22:37:28 +0900

    refactor: 배송지 수정/등록 form yup 스키마 수정

--- app/user/delivery/components/yupSchema.ts
+++ app/user/delivery/components/yupSchema.ts
@@ -1,39 +1,14 @@
 import * as yup from "yup";
 
 export const deliveryFormYupSchema = yup.object().shape({
-  recipient: yup.string().required("이름을 입력해주세요."),
-  address: yup
-    .object()
-    .shape({
-      full_address: yup.string().required("주소를 입력해주세요."),
-      zone_code: yup.string().required("주소를 입력해주세요."),
-    })
-    .test("address-validation", "주소를 입력해주세요.", (values) => {
-      const { full_address, zone_code } = values;
-
-      if (full_address && zone_code) return true;
-      else return false;
-    }),
-  detail_address: yup.string().required("상세주소를 입력해주세요."),
-  phone: yup
-    .object()
-    .shape({
-      first: yup.string(),
-      middle: yup.string(),
-      last: yup.string(),
-    })
-    .test(
-      "phone-validation",
-      "유효하지 않은 연락처 입니다.",
-      function (values) {
-        const { first, middle, last } = values;
-        if (!first || !middle || !last) {
-          return this.createError({ message: "연락처를 입력해주세요." });
-        }
-        if (middle.length < 3 || last.length < 4) {
-          return this.createError({ message: "유효하지 않은 연락처 입니다." });
-        }
-        return true;
-      }
-    ),
+  recipient: yup.string().required(),
+  address: yup.string().required(),
+  zone_code: yup.string().required(),
+  detail_address: yup.string(),
+  phone_prefix: yup.object(),
+  phone_start: yup.string().required().min(3),
+  phone_end: yup.string().required().min(4),
+  message_index: yup.object(),
+  direct_message: yup.string(),
+  is_default: yup.boolean(),
 });
