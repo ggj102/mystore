8faa303e4b1f6fe0e8d61220ba6e5f04208785cf
kjkim <kjkim@xsinc.co.kr>
2024-06-06 09:26:43 +0900

    refactor: [User] 주문내역 0건 에외처리

--- app/user/order/components/userOrder.tsx
+++ app/user/order/components/userOrder.tsx
@@ -39,42 +39,46 @@ export default function UserOrder({ data }: any) {
     <div className={userOrderStyle.user_order_container}>
       <div>
         <strong>주문내역</strong>
-        <ul>
-          {data.map((val: any, idx: number) => {
-            return (
-              <li key={idx}>
-                <div className="list_header">
-                  <span className={userOrderStyle.order_product_name}>
-                    {`${val.order_name} `}
-                    {val.remain > 1 && (
-                      <>
-                        외<span>3</span>건
-                      </>
-                    )}
-                  </span>
-                  <button onClick={() => onClickOrderDelete(val.id)}>
-                    <ImCross />
-                  </button>
-                </div>
-                <div className={userOrderStyle.order_product_info}>
-                  <div>
-                    <img src={val.image_path} alt="img" />
+        {data.length > 0 ? (
+          <ul>
+            {data.map((val: any, idx: number) => {
+              return (
+                <li key={idx}>
+                  <div className="list_header">
+                    <span className={userOrderStyle.order_product_name}>
+                      {`${val.order_name} `}
+                      {val.remain > 1 && (
+                        <>
+                          외<span>3</span>건
+                        </>
+                      )}
+                    </span>
+                    <button onClick={() => onClickOrderDelete(val.id)}>
+                      <ImCross />
+                    </button>
                   </div>
-                  <div className="text_wrap">
-                    <div>{`${dateFormmater(val.updated_at)} 결제`}</div>
+                  <div className={userOrderStyle.order_product_info}>
                     <div>
-                      결제금액: {priceFormatter(val.total_payment_price)}원
+                      <img src={val.image_path} alt="img" />
                     </div>
-                    <div>주문코드: {val.payment_key}</div>
+                    <div className="text_wrap">
+                      <div>{`${dateFormmater(val.updated_at)} 결제`}</div>
+                      <div>
+                        결제금액: {priceFormatter(val.total_payment_price)}원
+                      </div>
+                      <div>주문코드: {val.payment_key}</div>
+                    </div>
                   </div>
-                </div>
-                <Link href={`/orderComplete?order_id=${val.id}`}>
-                  주문 상세
-                </Link>
-              </li>
-            );
-          })}
-        </ul>
+                  <Link href={`/orderComplete?order_id=${val.id}`}>
+                    주문 상세
+                  </Link>
+                </li>
+              );
+            })}
+          </ul>
+        ) : (
+          <div className={userOrderStyle.empty}>주문내역이 없습니다.</div>
+        )}
       </div>
     </div>
   );
