eb7916001fbd8844d6618bf0029969830110d362
kjkim <kjkim@xsinc.co.kr>
2024-06-07 16:21:14 +0900

    refactor: [Order] payment 컴포넌트 결제 버튼 분리

--- app/order/components/payment.tsx
+++ app/order/components/payment.tsx
@@ -1,39 +1,16 @@
 "use client";
 
-import { useEffect, useRef, useState } from "react";
-import { useSearchParams } from "next/navigation";
-
-import { PaymentWidgetInstance } from "@tosspayments/payment-widget-sdk";
-import { loadPaymentWidget } from "@tosspayments/payment-widget-sdk";
-import { nanoid } from "nanoid";
-
-import { tokenExpiredErrorMessage } from "@/httpClient/errorMessage";
-import { priceFormatter } from "@/utils/priceFormatter";
-import { paymentAction } from "./orderActions";
+import { useEffect } from "react";
 
 import FoldContainer from "./foldContainer";
 
-import orderStyle from "@styles/pages/order/order.module.scss";
-
 export default function Payment({
-  userData,
-  orderItem,
-  totalPrice,
-  deliveryMessage,
+  widgetRef,
+  paymentWidget,
+  priceData,
+  setIsAgreement,
 }: any) {
-  const clientKey = process.env.NEXT_PUBLIC_TOSS_CLIENT_API_KEY;
-  const customerKey = process.env.NEXT_PUBLIC_TOSS_SECRET_API_KEY;
-
-  const { price, delivery } = totalPrice;
-
-  const [paymentWidget, setPaymentWidget] = useState<any>();
-
-  const searchParams = useSearchParams();
-  const orderId = searchParams.get("order_id");
-
-  const widgetRef = useRef<ReturnType<
-    PaymentWidgetInstance["renderPaymentMethods"]
-  > | null>(null);
+  const { price, delivery } = priceData;
 
   useEffect(() => {
     if (paymentWidget == null) {
@@ -52,8 +29,12 @@ export default function Payment({
 
     // ------  이용약관 렌더링 ------
     // @docs https://docs.tosspayments.com/reference/widget-sdk#renderagreement선택자-옵션
-    paymentWidget.renderAgreement("#agreement", {
+    const paymentAgreement = paymentWidget.renderAgreement("#agreement", {
       variantKey: "AGREEMENT",
+    });
+
+    paymentAgreement.on("change", (agreementStatus: any) => {
+      setIsAgreement(agreementStatus.agreedRequiredTerms);
     });
   }, [paymentWidget]);
 
@@ -66,60 +47,14 @@ export default function Payment({
     // ------ 금액 업데이트 ------
     // @docs https://docs.tosspayments.com/reference/widget-sdk#updateamount결제-금액
     paymentMethodsWidget.updateAmount(price + delivery);
-  }, [totalPrice]);
-
-  const onClickPayment = async () => {
-    const length = orderItem.length > 1 ? ` 외 ${orderItem.length - 1}건` : "";
-    const orderName = `${orderItem[0].name}${length}`;
-    const delivery_address = `${userData.user_address} ${userData.user_detail_address}`;
-
-    const paymentMethod = widgetRef.current?.getSelectedPaymentMethod();
-    const payment_method = paymentMethod?.easyPay
-      ? paymentMethod?.easyPay?.provider
-      : paymentMethod?.method;
-
-    const bodyData = {
-      order_name: orderName,
-      recipient: userData.user_name,
-      phone: userData.user_phone,
-      delivery_address,
-      delivery_message: deliveryMessage,
-      payment_method,
-    };
-
-    try {
-      await paymentAction(orderId, bodyData);
-      await paymentWidget?.requestPayment({
-        orderId: nanoid(),
-        orderName,
-        successUrl: `http://localhost:3005/order/success?order_id=${orderId}`,
-        failUrl: `${window.location.origin}/fail`,
-      });
-    } catch (err) {
-      tokenExpiredErrorMessage(err);
-    }
-  };
-
-  useEffect(() => {
-    loadPaymentWidget(clientKey, customerKey).then((widget) => {
-      setPaymentWidget(widget);
-    });
-  }, []);
+  }, [priceData]);
 
   return (
-    <>
-      <FoldContainer title="결제수단">
-        <div className="box_section">
-          <div id="payment-widget" style={{ width: "100%" }} />
-          <div id="agreement" style={{ width: "100%" }} />
-        </div>
-      </FoldContainer>
-      <div className={orderStyle.payment_button}>
-        <div>주문 내용을 확인하였으며 약관에 동의합니다.</div>
-        <button onClick={onClickPayment}>
-          {priceFormatter(price + delivery)}원 결제하기
-        </button>
+    <FoldContainer title="결제수단">
+      <div className="box_section">
+        <div id="payment-widget" style={{ width: "100%" }} />
+        <div id="agreement" style={{ width: "100%" }} />
       </div>
-    </>
+    </FoldContainer>
   );
 }
