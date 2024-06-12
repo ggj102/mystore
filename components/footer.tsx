5badb9caef44fb7d06d506993c4619653393bb7d
kjkim <kjkim@xsinc.co.kr>
2024-06-12 17:11:54 +0900

    feat: footer 프로토타입

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,5 +1,13 @@
 import footerStyles from "../styles/components/footer.module.scss";
 
 export default function Footer() {
-  return <div className={footerStyles.footer_container}>Footer</div>;
+  return (
+    <div className={footerStyles.footer_container}>
+      <div className="site_wrap">
+        <div className={footerStyles.footer_contents}>
+          MY STORE 명세서 | 이력서 | git | mail
+        </div>
+      </div>
+    </div>
+  );
 }
