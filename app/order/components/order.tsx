c27c6794d8b1c7068514742984e620301b526e2d
kjkim <kjkim@xsinc.co.kr>
2024-08-21 16:15:59 +0900

    design: [Order] 불필요한 문구 삭제

--- app/order/components/order.tsx
+++ app/order/components/order.tsx
@@ -176,7 +176,6 @@ export default function Order({
           setIsAgreement={setIsAgreement}
         />
         <div className={orderStyle.payment_button}>
-          <div>주문 내용을 확인하였으며 약관에 동의합니다.</div>
           <button type="submit" disabled={!isValid || !isAgreement}>
             {priceFormatter(priceData.price + priceData.delivery)}원 결제하기
           </button>
